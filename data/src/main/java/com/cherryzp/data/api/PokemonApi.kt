c50be7ffbed7d31aa3fcbc1bff56b67a3f1826d6
cherry <cherryzp9094@gmail.com>
2024-12-17 00:08:51 +0900

    feat: pokemonList api 구현

--- data/src/main/java/com/cherryzp/data/api/PokemonApi.kt
+++ data/src/main/java/com/cherryzp/data/api/PokemonApi.kt
@@ -1,7 +1,14 @@
 package com.cherryzp.data.api
 
+import com.cherryzp.data.model.PokemonListResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface PokemonApi {
 
+    @GET("pokemon")
+    suspend fun fetchPokemonList(
+        @Query("limit") limit: Int,
+        @Query("offset") offset: Int,
+    ): PokemonListResponse
 }
---
+++ data/src/main/java/com/cherryzp/data/model/PokemonResponse.kt
@@ -0,0 +1,6 @@
+package com.cherryzp.data.model
+
+data class PokemonResponse(
+    val name: String?,
+    val url: String?
+)
