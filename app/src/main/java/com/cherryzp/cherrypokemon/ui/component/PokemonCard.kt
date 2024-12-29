3863b461ce24721dc5d237d116532b0656eb9946
cherry <cherryzp9094@gmail.com>
2024-12-29 17:05:06 +0900

    feat: 팔레트 dependency 추가 및 포켓몬 색상에 따른 배경 추가

--- app/src/main/java/com/cherryzp/cherrypokemon/ui/component/PokemonCard.kt
+++ app/src/main/java/com/cherryzp/cherrypokemon/ui/component/PokemonCard.kt
@@ -1,5 +1,6 @@
 package com.cherryzp.cherrypokemon.ui.component
 
+import android.graphics.Bitmap
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
@@ -9,14 +10,24 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
 import com.skydoves.landscapist.glide.GlideImage
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 @Composable
 fun PokemonCard(
@@ -55,10 +66,37 @@ fun PokemonCard(
             modifier = Modifier
                 .clip(Shapes().medium)
                 .fillMaxWidth()
-                .aspectRatio(1f),
+                .aspectRatio(1f)
+                .background(extractDominantColor(image.orEmpty())),
             imageModel = { image }
         )
     }
+}
+
+@Composable
+fun extractDominantColor(url: String): Color {
+    var dominantColor by remember { mutableStateOf(Color.White) }
+    val context = LocalContext.current
+    LaunchedEffect(url) {
+        withContext(Dispatchers.IO) {
+            try {
+                val bitmap = Glide.with(context)
+                    .asBitmap()
+                    .load(url)
+                    .submit()
+                    .get()
+
+                val palette = Palette.from(bitmap).generate()
+                val color = palette.getDominantColor(0xFFFFFF)
+
+                dominantColor = Color(color)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    return dominantColor
 }
 
 @Preview
