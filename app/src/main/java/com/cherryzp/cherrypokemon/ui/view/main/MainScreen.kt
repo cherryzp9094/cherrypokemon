5cdf040a5550ca229e9efe60caf4c75610cdfb44
cherry <cherryzp9094@gmail.com>
2024-12-30 23:26:56 +0900

    style: 불필요한 import 제거

--- app/src/main/java/com/cherryzp/cherrypokemon/ui/view/main/MainScreen.kt
+++ app/src/main/java/com/cherryzp/cherrypokemon/ui/view/main/MainScreen.kt
@@ -2,16 +2,10 @@ package com.cherryzp.cherrypokemon.ui.view.main
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -19,7 +13,6 @@ import androidx.paging.compose.LazyPagingItems
 import com.cherryzp.cherrypokemon.ui.component.PokemonCard
 import com.cherryzp.data.extend.default
 import com.cherryzp.domain.model.Pokemon
-import com.skydoves.landscapist.glide.GlideImage
 
 @Composable
 fun MainScreen(
